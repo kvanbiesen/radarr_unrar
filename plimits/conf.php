27a4595da57ab5d8a4f4cb3f267daa0b48a7978a
kvanbiesen <kvanbiesen@gmail.com>
2018-03-10 19:37:51 +0100

    next fix to autoload

--- plimits/conf.php
+++ plimits/conf.php
@@ -6,8 +6,8 @@ $MAX_DL_LIMIT 	= 0;		// unlimited
 $RATIO_LIMIT	= 0;		// percents
 
 $trackersCheckInterval	= 1;	// in minutes
-$restrictedTrackersURL 	= 'http://cheapseedboxes.com/trackers.asd';
-
+//$restrictedTrackersURL 	= 'http://cheapseedboxes.com/trackers.asd';
+$restrictedTrackersURL 	= '/downloads/.rutorrent/settings/trackers.lst'
 $preventUpload	= true;
 
 $log_debug 		= true;		// Log info to /tmp/errors.log
